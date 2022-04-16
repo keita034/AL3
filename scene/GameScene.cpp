1a40bdde708c6635f76baab7eded7874a3ff5150
啓太 籾山 <k021g1266@g.neec.ac.jp>
2022-04-16 18:33:53 +0900

    02_01_ex課題3

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -23,13 +23,18 @@ void GameScene::Initialize() {
 	for (size_t i = 0; i < _countof(worldTransform_); i++) {
 
 		for (size_t j = 0; j < _countof(worldTransform_); j++) {
-			worldTransform_[i][j].scale_ = {1.0f, 1.0f, 1.0f};
 
-			//平行移動を設定
-			worldTransform_[i][j].translation_ = {-16.0f + j * 4.0f, -16.0f + i * 4.0f, 0};
+			for (size_t k = 0; k < _countof(worldTransform_); k++) {
 
-			//ワールドトランスフォーム初期化
-			worldTransform_[i][j].Initialize();
+				worldTransform_[i][j][k].scale_ = {1.0f, 1.0f, 1.0f};
+
+				//平行移動を設定
+				worldTransform_[i][j][k].translation_ = {
+				  -12.0f + j * 3.0f, -12.0f + i * 3.0f, 0.0f + k * 4.0f};
+
+				//ワールドトランスフォーム初期化
+				worldTransform_[i][j][k].Initialize();
+			}
 		}
 	}
 
@@ -69,18 +74,17 @@ void GameScene::Draw() {
 
 		for (size_t j = 0; j < _countof(worldTransform_); j++) {
 
-			if (i % 2 == 1&& j%2==1) {
-				continue;
-			}
+			for (size_t k = 0; k < _countof(worldTransform_); k++) {
 
-			model_->Draw(worldTransform_[i][j], viewProjection_, textureHandle_);
+				model_->Draw(worldTransform_[i][j][k], viewProjection_, textureHandle_);
+			}
 		}
 	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
-	 
+
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
 	Sprite::PreDraw(commandList);
