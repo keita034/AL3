9ba7a1056cc8352b458012e022f1308b7205aa98
keita_momiyama <93181795+keita034@users.noreply.github.com>
2022-06-06 12:50:28 +0900

    アフィン変換をワールド変換に移動

--- 3d/WorldTransform.h
+++ 3d/WorldTransform.h
@@ -4,6 +4,7 @@
 #include "Matrix4.h"
 #include <d3d12.h>
 #include <wrl.h>
+#include<cmath>
 
 // 定数バッファ用データ構造体
 struct ConstBufferDataWorldTransform {
@@ -13,17 +14,18 @@ struct ConstBufferDataWorldTransform {
 /// <summary>
 /// ワールド変換データ
 /// </summary>
-struct WorldTransform {
+struct WorldTransform
+{
 	// 定数バッファ
 	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
 	// マッピング済みアドレス
 	ConstBufferDataWorldTransform* constMap = nullptr;
 	// ローカルスケール
-	Vector3 scale_ = {1, 1, 1};
+	Vector3 scale_ = { 1, 1, 1 };
 	// X,Y,Z軸回りのローカル回転角
-	Vector3 rotation_ = {0, 0, 0};
+	Vector3 rotation_ = { 0, 0, 0 };
 	// ローカル座標
-	Vector3 translation_ = {0, 0, 0};
+	Vector3 translation_ = { 0, 0, 0 };
 	// ローカル → ワールド変換行列
 	Matrix4 matWorld_;
 	// 親となるワールド変換へのポインタ
@@ -45,4 +47,28 @@ struct WorldTransform {
 	/// 行列を転送する
 	/// </summary>
 	void TransferMatrix();
+
+	//スケーリング行列生成
+	Matrix4 ScalingForm();
+
+	// X軸回転行列を生成
+	Matrix4 RotationXForm();
+
+	// Y軸回転行列を生成
+	Matrix4 RotationYForm();
+
+	// Z軸回転行列を生成
+	Matrix4 RotationZForm();
+
+	// 回転行列を生成
+	Matrix4 RotationForm();
+
+	//平行移動行列を生成
+	Matrix4 TransferForm();
+
+	//ワールド行列を生成
+	Matrix4 WorldForm();
+
+	//アフィン変換
+	void AffineTransformation();
 };
