db025ede22e7de653a9a7d133a3dcae11191b4bc
keita_momiyama <93181795+keita034@users.noreply.github.com>
2022-06-07 14:01:26 +0900

    アフィン変換をワールド変換へ移動

---
+++ original/PlayerBullet.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Model.h"
+#include"Vector3.h"
+#include"WorldTransform.h"
+
+/// <summary>
+///　自キャラの弾
+/// </summary>
+class PlayerBullet {
+  public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標 </param>
+	/// /// <param name="velocity">速度 </param>
+	void Initialize(Model* model, const Vector3& position,const Vector3& velocity);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// /// <param name="viewProjection">ビュープロジェクション</param>
+	void Draw(const ViewProjection viewProjection);
+
+	///デスフラグゲッター
+	bool IsDead() const;
+
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t texturehandle_ = 0u;
+
+	//速度
+	Vector3 velocity_;
+
+	//寿命<frm>
+	static const int32_t kLifeTime = 60 * 5;
+
+	//デスタイマー
+	int32_t deathTimer_ = kLifeTime;
+	//デスフラグ
+	bool isDead_ = false;
+
+};
+
+inline bool PlayerBullet::IsDead() const
+{
+	return isDead_;
+}
